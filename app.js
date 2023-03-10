7dbde746164ea4135d78a724cecf5d9c4fb379a3
Adem <ademduran6888@gmail.com>
2023-03-10 10:44:25 +0100

    feat(routing): implementing simple routing

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("Accueil");
+});
+
+app.get('/produits', (req, res) => {
+  res.send("Catalogue produit");
+});
+
+app.get('/produits/:id(\\d+)', (req, res) => {
+  console.log(req.query);
+  res.send("Catalogue de produits");
+});
+
+app.get('/api/*', (req, res) => {
+  res.send("API...");
+});
+
+app.listen(port, () => {
+  console.log("Serveur démaarré sur http://localhost:" + port);
+});
